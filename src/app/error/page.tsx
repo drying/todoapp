15c1c4001b74328082aca9bc7689dc0f432358c8
yukimatsu <drying3@gmail.com>
2024-04-28 12:52:27 +0900

    ✨ ブランチを切るために一旦コミット

---
+++ src/app/error/page.tsx
@@ -0,0 +1,3 @@
+export default function Errorpage() {
+  return <p>Sorry, something went wrong</p>;
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,39 @@
 import Header from "../component/Header";
+import Title from "../component/Title";
+import { login } from "./action";
 
 export default function Login() {
   return (
     <>
       <Header title="HOME" leftMenu="ログイン" rightMenu="新規登録" />
       <div className="flex min-h-full flex-col items-center px-6 py-12">
-        <div>
-          <h1 className="text-2xl text-center mb-4">Todoアプリ</h1>
-          <h2 className="text-xl text-center mb-4">ログイン</h2>
-        </div>
+        <Title title="ログイン" />
 
         <div className="flex flex-col border p-8">
           <div className="pb-4">
             <label className="block text-sm font-medium">Email address</label>
-            <input type="email" className="border" />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="border"
+              required
+            />
           </div>
           <div className="pb-4">
             <label className="block text-sm font-medium">Password</label>
-            <input type="password" className="border" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              className="border"
+              required
+            />
           </div>
           <div>
             <button
               type="submit"
+              formAction={login}
               className="flex w-full justify-center rounded-md px-3 py-1 mb-2 bg-blue-500 text-white text-sm"
             >
               ログイン
