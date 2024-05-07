402763d0ce1b43d9e1c553fdcc83f4c00a8d554f
yukimatsu <drying3@gmail.com>
2024-05-07 21:39:07 +0900

    ✨ ToDo削除機能

--- src/app/component/TodoList.tsx
+++ src/app/component/TodoList.tsx
@@ -1,11 +1,19 @@
+import React from "react";
 import { Task } from "../../../utils/interface";
+import { deleteTodo } from "../../../utils/supabase/supabaseFunc";
 
 type Props = {
   tasks: Task[];
+  getTodos: () => void;
 };
 
 export default function TodoList(props: Props) {
-  const { tasks } = props;
+  const { tasks, getTodos } = props;
+
+  const onClickDelete = async (id: number) => {
+    await deleteTodo(id);
+    getTodos(); // Todoを呼び出して画面更新する
+  };
 
   return (
     <>
@@ -18,7 +26,10 @@ export default function TodoList(props: Props) {
             >
               <li className="text-gray-700">{task.title}</li>
               <div className="flex space-x-2">
-                <button className="px-3 py-2 bg-red-400 text-gray-100 text-sm shadow-md">
+                <button
+                  onClick={() => onClickDelete(task.id)}
+                  className="px-3 py-2 bg-red-400 text-gray-100 text-sm shadow-md"
+                >
                   削除
                 </button>
                 <button className="px-3 py-2 bg-blue-400 text-gray-100 text-sm shadow-md">
--- utils/supabase/supabaseFunc.ts
+++ utils/supabase/supabaseFunc.ts
@@ -1,10 +1,22 @@
 import { supabase } from "./supabase";
 
+//ToDo todo関連のCRUDはここにまとめて記載して各ファイルで呼び出すように修正する
+// すべてのtodoを取得する
 export const getAllTodos = async () => {
   const tasks = await supabase.from("tasks").select("*");
   return tasks.data;
 };
-
+// ToDoを追加する
+//ToDo user_idを取得して引数に追加する
 export const addTodo = async (title: string) => {
   await supabase.from("tasks").insert({ title: title }).select();
 };
+
+// Todoを削除する
+export const deleteTodo = async (id: number) => {
+  await supabase.from("tasks").delete().eq("id", id);
+};
+
+// Todoを編集する
+
+// Todoを完了する
