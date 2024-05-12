3701eee5109c9619ecf89c07a3026d8c49c08887
yukimatsu <drying3@gmail.com>
2024-05-12 22:01:01 +0900

    ♻️ user_idで表示を変える用に修正

--- src/app/component/TodoList.tsx
+++ src/app/component/TodoList.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Task } from "../../../utils/interface";
 import {
   deleteTodo,
   doneTodo,
   editTodo,
+  getUserID,
 } from "../../../utils/supabase/supabaseFunc";
 
 type Props = {
@@ -15,6 +16,18 @@ export default function TodoList(props: Props) {
   const { tasks, getTodos } = props;
   const [editId, setEditId] = useState<number | null>(null);
   const [editTitle, setEditTitle] = useState<string>("");
+  const [userId, setUserID] = useState<string | null>(null);
+
+  // user_idを取得する
+  useEffect(() => {
+    const fetchUserID = async () => {
+      const id = await getUserID();
+      if (id) {
+        setUserID(id);
+      }
+    };
+    fetchUserID();
+  }, []);
 
   const onClickDelete = async (id: number) => {
     await deleteTodo(id);
@@ -42,57 +55,59 @@ export default function TodoList(props: Props) {
     <>
       <form className="w-full max-w-xl">
         <ul className="space-y-3">
-          {tasks.map((task) => (
-            <div
-              key={task.id}
-              className="flex justify-between space-x-4 p-4 items-center bg-white shadow-md rounded-lg"
-            >
-              <input
-                className="flex-none h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                type="checkbox"
-                onChange={() => onClickDone(task.id, task.is_complete)}
-                checked={task.is_complete}
-              />
-              {editId === task.id ? (
-                <>
-                  <input
-                    type="text"
-                    placeholder="新しいTodoを追加"
-                    className="w-5/6 px-12 py-3 mb-4 bg-white shadow-md rounded-lg"
-                    onChange={(e) => setEditTitle(e.target.value)}
-                    value={editTitle}
-                  />
-                  <button
-                    type="button"
-                    onClick={() => handleEdit(task.id)}
-                    className="px-3 py-2 bg-blue-400 text-gray-100 text-sm shadow-md"
-                  >
-                    完了
-                  </button>
-                </>
-              ) : (
-                <>
-                  <li className="flex-1 text-gray-700">{task.title}</li>
-                  <div className="flex space-x-2">
+          {tasks.map((task) =>
+            userId === task.user_id ? (
+              <div
+                key={task.id}
+                className="flex justify-between space-x-4 p-4 items-center bg-white shadow-md rounded-lg"
+              >
+                <input
+                  className="flex-none h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  type="checkbox"
+                  onChange={() => onClickDone(task.id, task.is_complete)}
+                  checked={task.is_complete}
+                />
+                {editId === task.id ? (
+                  <>
+                    <input
+                      type="text"
+                      placeholder="新しいTodoを追加"
+                      className="w-5/6 px-12 py-3 mb-4 bg-white shadow-md rounded-lg"
+                      onChange={(e) => setEditTitle(e.target.value)}
+                      value={editTitle}
+                    />
                     <button
                       type="button"
-                      onClick={() => onClickDelete(task.id)}
-                      className="px-3 py-2 bg-red-400 text-gray-100 text-sm shadow-md"
-                    >
-                      削除
-                    </button>
-                    <button
-                      type="button"
-                      onClick={() => onClickEdit(task.id)}
+                      onClick={() => handleEdit(task.id)}
                       className="px-3 py-2 bg-blue-400 text-gray-100 text-sm shadow-md"
                     >
-                      編集
+                      完了
                     </button>
-                  </div>
-                </>
-              )}
-            </div>
-          ))}
+                  </>
+                ) : (
+                  <>
+                    <li className="flex-1 text-gray-700">{task.title}</li>
+                    <div className="flex space-x-2">
+                      <button
+                        type="button"
+                        onClick={() => onClickDelete(task.id)}
+                        className="px-3 py-2 bg-red-400 text-gray-100 text-sm shadow-md"
+                      >
+                        削除
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => onClickEdit(task.id)}
+                        className="px-3 py-2 bg-blue-400 text-gray-100 text-sm shadow-md"
+                      >
+                        編集
+                      </button>
+                    </div>
+                  </>
+                )}
+              </div>
+            ) : null
+          )}
         </ul>
       </form>
     </>
--- utils/interface.ts
+++ utils/interface.ts
@@ -1,5 +1,6 @@
 export interface Task {
   id: number;
+  user_id: string;
   title: string;
   created_at: string;
   is_complete: boolean;
